e2bc2693fefc612211f519cb247a5b96b8933f3e
Mhd Sulhan <ms@kilabit.info>
2016-02-21 23:21:25 +0700

    Add a list of vulgar words.

---
+++ words_list_test.go
@@ -0,0 +1,30 @@
+// Copyright 2016 Mhd Sulhan <ms@kilabit.info>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tekstus_test
+
+import (
+	"github.com/shuLhan/tekstus"
+	"testing"
+)
+
+var dataVulgarWords = []struct {
+	text string
+	exp  float64
+}{
+	{
+		"Canadian ", 1.0,
+	}, {
+		" (partition)|Bulkhead", 2.0,
+	},
+}
+
+func TestVulgarWords(t *testing.T) {
+	for _, td := range dataVulgarWords {
+		got := tekstus.StringFrequenciesOf(td.text,
+			tekstus.VulgarWords, false)
+
+		assert(t, td.exp, got, true)
+	}
+}
